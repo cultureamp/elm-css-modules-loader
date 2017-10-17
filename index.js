20e500e4d7d9c562a943bbae7a2ede722dcc5070
Jason O'Neil <jason.oneil@gmail.com>
2017-10-18 06:57:54 +1100

    Use a regex rather than Babel for the transformation.
    This feels like a step backwards (I'd almost always prefer parsing to regex transforms), but this loader performs a very simple transformation on very large JS output, and is necessary for build performance on large elm projects.
    
    Reduces build time:
    
    - In the example project
         - From ~2000ms
         - To ~900ms
    - In Culture Amp's app build, no cache
        - No cache: From 74s to 68s
        - Cache, full build: From 9.4s to 8.9s
        - Dev server rebuild: From 6.3s to 5.5s

--- index.js
+++ index.js
@@ -1,103 +1,49 @@
 const loaderUtils = require('loader-utils');
-const babel = require('babel-core');
-const { default: generate } = require('babel-generator');
 
 const loader = function(source, inputSourceMap) {
-  this.cacheable && this.cacheable(); // for Webpack 1.x compatibility
+  if (this.cacheable) this.cacheable();
 
   const config = loaderUtils.getOptions(this) || {};
+  const packageName = config.package || 'cultureamp/elm-css-modules-loader';
+  config.module = config.module || 'CssModules';
+  config.tagger = config.tagger || 'css';
 
-  config.module = config['module'] || 'CssModules';
-  config.tagger = config['tagger'] || 'css';
-
-  const packageName = config['package'] || 'cultureamp/elm-css-modules-loader';
   const taggerName =
-    '_' +
-    [
-      packageName.replace(/-/g, '_').replace(/\//g, '$'),
-      config.module.replace(/\./g, '_'),
-      config.tagger,
-    ].join('$');
-
-  const transformerOptions = {
-    taggerName: taggerName,
-    localPath: config.localPath,
-  };
-
-  const webpackRemainingChain = loaderUtils
-    .getRemainingRequest(this)
-    .split('!');
-  const filename = webpackRemainingChain[webpackRemainingChain.length - 1];
-  const babelOptions = {
-    inputSourceMap: inputSourceMap,
-    sourceRoot: process.cwd(),
-    filename: filename,
-    compact: false,
-    babelrc: false,
-  };
-
-  const result = transform(source, this, transformerOptions, babelOptions);
-
-  this.callback(null, result.code, result.map);
-};
-
-function transform(source, loaderContext, transformerOptions, babelOptions) {
-  babelOptions.plugins = [
-    cssModulesTransformer(loaderContext, transformerOptions),
-  ];
-
-  const { code, map } = babel.transform(source, babelOptions);
-
-  if (map && (!map.sourcesContent || !map.sourcesContent.length)) {
-    map.sourcesContent = [source];
-  }
-
-  return { code, map };
-}
-
-function cssModulesTransformer(loaderContext, options) {
-  return function plugin({ types: t }) {
-    return {
-      visitor: {
-        CallExpression: callExpressionVisitor(t, loaderContext, options),
-      },
-    };
-  };
-}
-
-const REPLACED_NODE = Symbol('elmCssModulesLoaderReplaced');
-
-function callExpressionVisitor(t, loaderContext, options) {
-  return function visitor(path) {
-    // avoid infinite recursion
-    if (path.node[REPLACED_NODE]) {
-      return;
-    }
-
-    // look for:
-    //
-    //     A2(<taggerName>, ...);
-    if (
-      !(t.isIdentifier(path.node.callee) &&
-        path.node.callee.name === 'A2' &&
-        t.isIdentifier(path.node.arguments[0]) &&
-        path.node.arguments[0].name === options.taggerName)
-    )
-      return;
-
-    // replace hard-coded CSS classes with require("stylesheet.css")
-    const replacement = t.callExpression(
-      path.node.callee, // A2 (leave alone)
+      '_' +
       [
-        path.node.arguments[0], // taggerName (leave alone)
-        path.node.arguments[1], // e.g. "stylesheet.css" (leave alone)
-        t.callExpression(t.Identifier('require'), [path.node.arguments[1]]),
-      ]
-    );
-    replacement[REPLACED_NODE] = true;
+        packageName.replace(/-/g, '_').replace(/\//g, '$'),
+        config.module.replace(/\./g, '_'),
+        config.tagger,
+      ].join('$'),
+    escapedTaggerName = taggerName.replace(/\$/g, '\\$'),
+    a2 = 'A2',
+    optionalWhitespace = '\\s*',
+    moduleNameCapture = '([a-z-./]+)',
+    expectedClassRules = "{[a-z0-9:, ']*}";
 
-    path.replaceWith(replacement);
-  };
-}
+  const regexp = new RegExp(
+    [
+      'A2\\(',
+      optionalWhitespace,
+      escapedTaggerName,
+      ',',
+      optionalWhitespace,
+      "'",
+      moduleNameCapture,
+      "',",
+      optionalWhitespace,
+      expectedClassRules,
+      optionalWhitespace,
+      '\\);',
+    ].join(''),
+    'gi'
+  );
+
+  const transformedSource = source.replace(
+    regexp,
+    'A2(' + taggerName + ", '$1', require('$1'));"
+  );
+  return transformedSource;
+};
 
 module.exports = loader;
