ce82bb35f7dbda68bf0e4566feeb52e1aa4e7812
Jason O'Neil <jason.oneil@gmail.com>
2018-06-27 16:59:57 +0800

    fix(ci): Use helper to get authorised URL for git remote

--- ci/publishElmRelease.js
+++ ci/publishElmRelease.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const execSync = require('child_process').execSync;
 const { tag, push } = require('semantic-release/lib/git');
+const getGitAuthUrl = require('semantic-release/lib/get-git-auth-url');
 
 /**
  A semantic release "publish" plugin to create tags and run `elm-package publish`.
@@ -13,9 +14,8 @@ async function tagElmRelease(config, context) {
 
   const elmPackageJson = JSON.parse(fs.readFileSync('elm-package.json'));
   await tag(elmPackageJson.version);
-
-  const packageJson = JSON.parse(fs.readFileSync('package.json'));
-  await push(packageJson.repository, config.branch);
+  const repositoryUrl = await getGitAuthUrl(config);
+  await push(repositoryUrl, config.branch);
 
   exec(`elm-package publish`);
 
